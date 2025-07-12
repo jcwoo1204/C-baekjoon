2a4ec426b870804e42e9d4bb2ea8707b77bbc211
jcwoo1204 <jcwoo1204@gmail.com>
2025-07-12 15:54:29 +0900

    fuck

--- 7. 2차원 배열/2738.c
+++ 7. 2차원 배열/2738.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 
+void Addarr(int rows, int cols) {
+    int arr[rows][cols];  // 2차원 배열 생성
+
+    // 배열 2개 입력받기
+
+    
+    for(int j = 0; j < rows; j++) {
+        for(int k = 0; k < cols; k++) {
+            scanf("%d", &arr[j][k]);
+        }
+    }
+    for(int j = 0; j < rows; j++) {
+        for(int k = 0; k < cols; k++) {
+            int a = 0;
+            scanf("%d", &a);
+            arr[j][k] += a;
+        }
+    }
+    
+
+    for(int j = 0; j < rows; j++) {
+            for(int k = 0; k < cols; k++) {
+                printf("%d ", arr[j][k]);
+            }
+            printf("\n");
+        }  
+}
+
 int main(void) {
 
     int N, M;
     scanf("%d %d", &N, &M);
 
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < N; j++) {
-            
-        }
-    }
+    Addarr(N, M);
 
     return 0;
 }
